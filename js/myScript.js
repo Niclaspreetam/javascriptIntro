7c53bd0aa476955922f0592b55b344cc7b953c29
Niclas Preetam Madsen <Niclas.madsen17@gmail.com>
2018-02-08 13:58:10 +0100

    j

--- js/myScript.js
+++ js/myScript.js
@@ -5,13 +5,20 @@
  **/
 
 // variabler
+var vand = "vand"; //tekststreng
+var vandIgen = 'vand';
+var vandIgennejn = 'vand "vandmand"';
 var tal = 111;
 var erDetRigtigt = false;
+var JulemandenFindes = true;
 var enStreng = "hej verden";
 
 // klister (+)
+
+var total = vand + vandIgennejn;
 // kommentarer
 // regning
+var talMere = 999;
 // datoer
 // lister (scratch: data)
 // hvis ... så ... (scratch: operators)
@@ -21,9 +28,9 @@ var enStreng = "hej verden";
 // noget her
 
 /* se resultatet af din kode i browserens udviklings-værktøj */
-console.log( enStreng  );
+console.log( tal + talMere  );
 // skriv i browservinduet
-
+document.write("<h1>" + "mojn, mojn do! " + vand) + "</h1>";
 /* se resultatet af din kode, der hvor scriptet ligger i koden */
 
 // næste gang: events.
